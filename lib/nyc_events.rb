798dfa4c2e5be1759e1d63ae95ee25fae72c40eb
Becky Phan <beckyphan@live.com>
2019-07-15 21:58:44 +0000

    updated bin/NYC-events and program runs

--- lib/nyc_events.rb
+++ lib/nyc_events.rb
@@ -3,3 +3,5 @@ require_relative "NYC_events/scraper.rb"
 require_relative "NYC_events/event.rb"
 require_relative "NYC_events/list.rb"
 require_relative "NYC_events/CLI.rb"
+
+# require_all "NYC_events"
